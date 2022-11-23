002cdd14e8e6a30782bb867504215c16b02df6f7
Asritha Bodepudi <azb0222@dexcom.com>
2022-11-23 00:35:57 -0800

    commiting suicide

--- geneva/src/components/Navbar.js
+++ geneva/src/components/Navbar.js
@@ -1,13 +1,58 @@
 import React from 'react';
-import {logo} from "../assets/logo.png"; 
-import {navLinks} from "../constants.js"; 
+import logo from "./logo.png";
+import { Menu } from '@headlessui/react';
+import { useState } from 'react'
+
+// import {navLinks} from "../constants.js"; 
+
+//move this to constants.js 
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'Developers', href: '#' },
+  { name: 'About Us', href: '#' },
+  { name: 'Blog', href: '#' },
+]
+
 const Navbar = props => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+
   return (
-    <div className="isolate bg-black">
-    <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-  
+    <div class="flex z-0">
+      <svg class="z-0 relative w-full inset-x-0 top-0" viewBox="0 0 303 88">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <polygon id="Path" fill="#D8D8D8" points="0 0 0 87.890625 303 27.3398438 303 0"></polygon>
+        </g>
+      </svg>
+
+      <div class= "container mx-auto p-6 z-10 absolute">
+        <div class="flex items-center justify-between">
+          <div class="flex pt-2 relative container space-x-2 mx-auto">
+            <img src={logo} class="w-8 h-8" />
+            <h1 class="place-self-center text-lg">Geneva</h1>
+          </div>
+
+          <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="font-heavy text-gray-900 hover:text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+
+        </div>
+
+        <div className=" lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+          <a
+            href="#"
+            className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+          >
+            Log in
+          </a>
+        </div>
+      </div>
     </div>
-    </div>
+
   );
 };
 
--- geneva/tailwind.config.js
+++ geneva/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   //put in purge command here 
@@ -7,13 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        genevaPurple: "#515ADB", 
+        genevaGrey: "#D8D8D8", 
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        avenir: ["Avenir", "sans-serif"],
       },
     },
     screens: {
